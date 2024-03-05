b211f265b72257cbb0d8ee27c7a081c540005c2d
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-05 14:48:30 -0600

    Add Comments for Info on ScreenBreaks

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,9 @@ import TicketCard from './(components)/TicketCard';
 const Dashboard = () => {
     return (
         <div className="p-5">
-            {/* anything over a large screen, 2col grid; 
-            anything over xl screen, 4col grid */}
-            <div className="lg:grid-cols-2 xl:grid-cols-4">
+            {/* anything over a large screen, 2col grid; anything over xl screen, 4col grid.
+             must set the initial breakpoint (lg:grid) for any additional formatting to happen*/}
+            <div className="grid-cols-2 lg:grid xl:grid-cols-4">
                 <TicketCard />
                 <TicketCard />
                 <TicketCard />
