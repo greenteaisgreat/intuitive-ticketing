abb2054479decdd7963a7a70437f8558dbaefd02
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-05 15:43:05 -0600

    Add NextResponse Error Handling

--- app/(server)/db.js
+++ app/(server)/db.js
@@ -1,10 +1,19 @@
 import mongoose from 'mongoose';
+import { NextResponse } from 'next/server';
 
 export default async function connectToDB() {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log('Connected to database!');
     } catch (err) {
-        console.log('There was an error connecting to the database ', err);
+        return NextResponse.json(
+            {
+                message: 'There was an error connecting to the database ',
+                error: err,
+            },
+            {
+                status: 500,
+            }
+        );
     }
 }
