8f99bfae9f70d307223564516d819633928fd9bb
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-05 11:41:01 -0600

    Add ProgressDisplay Component

---
+++ app/(components)/ProgressDisplay.jsx
@@ -0,0 +1,12 @@
+const ProgressDisplay = () => {
+    return (
+        <div className="my-2 h-2.5 w-full rounded-md bg-gray-400">
+            <div
+                className="h-2.5 rounded-md bg-blue-600"
+                style={{ width: '65%' }}
+            ></div>
+        </div>
+    );
+};
+
+export default ProgressDisplay;
