553e2eb4c8d0da1bbfa0f37b0e5b12d5e31996ad
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-05 12:14:08 -0600

    Remove Vertical Margin

--- app/(components)/ProgressDisplay.jsx
+++ app/(components)/ProgressDisplay.jsx
@@ -1,6 +1,6 @@
 const ProgressDisplay = () => {
     return (
-        <div className="my-2 h-2.5 w-full rounded-md bg-gray-400">
+        <div className="h-2.5 w-full rounded-md bg-gray-400">
             <div
                 className="h-2.5 rounded-md bg-blue-600"
                 style={{ width: '65%' }}
