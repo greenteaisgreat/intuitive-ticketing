e7503721007ca9ab4997c37ccb658a7644262c37
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-05 14:04:05 -0600

    Set Screen Breakpoints for Ticket Displays

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,15 @@ import TicketCard from './(components)/TicketCard';
 
 const Dashboard = () => {
     return (
-        <div>
-            <TicketCard />
-            <TicketCard />
-            <TicketCard />
+        <div className="p-5">
+            {/* anything over a large screen, 2col grid; 
+            anything over xl screen, 4col grid */}
+            <div className="grid-cols-2 lg:grid xl:grid-cols-4">
+                <TicketCard />
+                <TicketCard />
+                <TicketCard />
+                <TicketCard />
+            </div>
         </div>
     );
 };
