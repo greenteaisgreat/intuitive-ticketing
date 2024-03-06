4e6871b12677e2cfbc0c8b67449fa7dbda5cc847
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-06 12:04:12 -0600

    Add TicketForm Component & Import Necessary Hooks

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+
+const TicketForm = () => {
+    return <div>TicketForm</div>;
+};
+
+export default TicketForm;
