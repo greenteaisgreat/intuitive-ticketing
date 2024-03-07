1badc2a105e1a99ec043d5e7a51e8036a1a57e35
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-06 20:28:15 -0600

    Add TicketForm State & Baseline Ticket Structure

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -6,7 +6,44 @@ import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 const TicketForm = () => {
-    return <div>TicketForm</div>;
+    const startingTicketData = {
+        title: '',
+        description: '',
+        category: '',
+        priority: 1,
+        progress: 0,
+        status: 'Not Started',
+        active: false,
+    };
+
+    const [formData, setFormData] = useState(startingTicketData);
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    return (
+        <div className="flex justify-center">
+            <form method="POST">
+                <h3>Create Your Ticket</h3>
+                <label htmlFor="title">Title</label>
+                <input
+                    id="title"
+                    name="title"
+                    type="text"
+                    onChange={handleChange}
+                    required={true}
+                    value={formData.title}
+                />
+            </form>
+        </div>
+    );
 };
 
 export default TicketForm;
