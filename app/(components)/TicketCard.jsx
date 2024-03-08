dd857e21cb1469581e2cd3c63805e44a4765313f
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-08 15:26:56 -0600

    Add Formatted Dates for TicketCards

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,35 +3,48 @@ import PriorityDisplay from './PriorityDisplay';
 import ProgressDisplay from './ProgressDisplay';
 import StatusDisplay from './StatusDisplay';
 
-const TicketCard = () => {
+const TicketCard = ({ ticket }) => {
+    const formatTimestamp = (timestamp) => {
+        //takes advantage of toLocaleString's options; the values are recognized
+        const options = {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: true,
+        };
+
+        const date = new Date(timestamp);
+        const formattedDate = date.toLocaleString('en-us', options);
+
+        return formattedDate;
+    };
+
     return (
         <div className="m-3 flex flex-col rounded-lg bg-card-400 p-3 shadow-xl hover:bg-card-500">
             <div className="mb-3 flex">
-                <PriorityDisplay />
+                <PriorityDisplay priority={ticket.priority} />
                 <div className="ml-auto">
                     <DeleteBlock />
                 </div>
             </div>
-            <h4>Ticket Title</h4>
+            <h4>{ticket.title}</h4>
             <hr className="mb-2 h-px border-0 bg-page-500" />
             {/* pre-wrap preserves \n, \r & spaces */}
-            <p className="whitespace-pre-wrap">
-                This is a description and I really need this fixed by the end of
-                the day, please and thank-you. Please, I really need this fixed!
-            </p>
+            <p className="whitespace-pre-wrap">{ticket.description}</p>
             {/* this empty flex-grow div ensures that all ticket cards will be 
             the same size, regardless of how much text content they contain */}
             <div className="flex-grow"></div>
             <div className="mt-2 flex">
                 <div className="flex flex-col">
                     <p className="my-1 text-xs">
-                        {new Date().toDateString()}&nbsp; | &nbsp;
-                        {new Date().toLocaleTimeString()}
+                        {formatTimestamp(ticket.createdAt)}
                     </p>
-                    <ProgressDisplay />
+                    <ProgressDisplay progress={ticket.progress} />
                 </div>
                 <div className="ml-auto flex items-end">
-                    <StatusDisplay />
+                    <StatusDisplay status={ticket.status} />
                 </div>
             </div>
         </div>
