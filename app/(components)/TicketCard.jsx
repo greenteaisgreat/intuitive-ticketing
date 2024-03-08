23ef120692bc411ce6be495003e2db8ffbf73a39
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-03-08 17:03:31 -0600

    Add Clickable TicketCard Based on ID

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import DeleteBlock from './DeleteBlock';
 import PriorityDisplay from './PriorityDisplay';
 import ProgressDisplay from './ProgressDisplay';
@@ -29,24 +30,30 @@ const TicketCard = ({ ticket }) => {
                     <DeleteBlock id={ticket._id} />
                 </div>
             </div>
-            <h4>{ticket.title}</h4>
-            <hr className="mb-2 h-px border-0 bg-page-500" />
-            {/* pre-wrap preserves \n, \r & spaces */}
-            <p className="whitespace-pre-wrap">{ticket.description}</p>
-            {/* this empty flex-grow div ensures that all ticket cards will be 
-            the same size, regardless of how much text content they contain */}
-            <div className="flex-grow"></div>
-            <div className="mt-2 flex">
-                <div className="flex flex-col">
-                    <p className="my-1 text-xs">
-                        {formatTimestamp(ticket.createdAt)}
-                    </p>
-                    <ProgressDisplay progress={ticket.progress} />
+            <Link
+                href={`/TicketPage/${ticket._id}`}
+                //tells CSS to ignore <Link> and only display the URL
+                style={{ display: 'contents' }}
+            >
+                <h4>{ticket.title}</h4>
+                <hr className="mb-2 h-px border-0 bg-page-500" />
+                {/* pre-wrap preserves \n, \r & spaces */}
+                <p className="whitespace-pre-wrap">{ticket.description}</p>
+                {/* this empty flex-grow div ensures that all ticket cards will be 
+                the same size, regardless of how much text content they contain */}
+                <div className="flex-grow"></div>
+                <div className="mt-2 flex">
+                    <div className="flex flex-col">
+                        <p className="my-1 text-xs">
+                            {formatTimestamp(ticket.createdAt)}
+                        </p>
+                        <ProgressDisplay progress={ticket.progress} />
+                    </div>
+                    <div className="ml-auto flex items-end">
+                        <StatusDisplay status={ticket.status} />
+                    </div>
                 </div>
-                <div className="ml-auto flex items-end">
-                    <StatusDisplay status={ticket.status} />
-                </div>
-            </div>
+            </Link>
         </div>
     );
 };
